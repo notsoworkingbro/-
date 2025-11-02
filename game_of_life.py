8a1b8be78da2cafbf092582e47cb04406c64d1c3
notsoworkingbro <workingamap@gmail.com>
2025-11-02 23:34:07 +0800

    ???

--- game_of_life.py
+++ game_of_life.py
@@ -4,4 +4,12 @@ class baby:
         self.middle_name = "joe"
         self.surname = "joe"
         
-        self.size = 3
+        self.sex = "random"
+        self.date = "date"
+        self.time_of_birth = "time"
+
+        self.place_of_birth_hospital = "a"
+        self.place_of_birth_city = "a"
+        self.place_of_birth_municipality = "a"
+        self.place_of_birth_province = "a"
+        self.place_of_birth_country = "a"
