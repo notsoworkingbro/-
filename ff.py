9bb7b734c57a84f29fa35fd5d73d829a45f118cd
notsoworkingbro <workingamap@gmail.com>
2025-11-07 23:05:21 +0800

    ???

---
+++ ff.py
@@ -0,0 +1,12 @@
+class fibo():
+    def fibonacci(n):
+        if n <= 0:
+            return "ss"
+        
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+        
+n = 10
+i = 1
+
+while i <== n:
